fb8f044a3fa173154ded7684a16e3587a366eeb8
ishrath401 <85433131+ishrath401@users.noreply.github.com>
2024-02-01 09:20:53 -0600

    Add files via upload

--- humbi/src/automate.java
+++ humbi/src/automate.java
@@ -1,7 +1,9 @@
 import java.io.File;  
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;  
@@ -17,7 +19,7 @@ class automate
 	{  
 		try  
 		{  
-			String path = "C:\\Users\\ishra\\eclipse-workspace\\humbi\\src\\InputDataSet.xlsx";
+			String path = "C:\\Users\\mohammed.ishrath\\git\\Automate\\humbi\\src\\InputDataSet.xlsx";
 			File file = new File(path);   //creating a new file instance  
 			FileInputStream fis = new FileInputStream(file);   //obtaining bytes from the file  
 			//creating Workbook instance that refers to .xlsx file  
@@ -25,15 +27,13 @@ class automate
 			XSSFSheet sheet = wb.getSheetAt(0);     //creating a Sheet object to retrieve object  
 			Iterator<Row> itr = sheet.iterator();    //iterating over excel file  
 			Workbook workbook = new HSSFWorkbook();
-			Sheet opsheet = workbook.createSheet("Sheet1");
-			
-			int i = 0;
-			Row oprow = opsheet.createRow(i);
-			Cell opcell = oprow.createCell(0);
+
+			List<List<Integer>> list = new ArrayList<>();
 			
 			
 			while (itr.hasNext())                 
 			{  
+				List<Integer> subList = new ArrayList<>();
 				Row row = itr.next();  
 				Iterator<Cell> cellIterator = row.cellIterator();   //iterating over each column  
 				while (cellIterator.hasNext())   
@@ -41,24 +41,49 @@ class automate
 					Cell cell = cellIterator.next();  
 					switch (cell.getCellType())               
 					{  
-					case Cell.CELL_TYPE_STRING:    //field that represents string cell type  
-						System.out.print(cell.getStringCellValue() + "\t\t\t");
-						opcell.setCellValue(cell.getStringCellValue());
-						oprow = opsheet.createRow(i++);
-						opcell = oprow.createCell(0);
+					case Cell.CELL_TYPE_STRING:
 						break;  
-					case Cell.CELL_TYPE_NUMERIC:    //field that represents number cell type  
-						System.out.print(cell.getNumericCellValue() + "\t\t\t");
-						opcell.setCellValue(cell.getNumericCellValue());
-						oprow = opsheet.createRow(i++);
-						opcell = oprow.createCell(0);
+					case Cell.CELL_TYPE_NUMERIC:    
+						int x = (int) cell.getNumericCellValue();
+						subList.add(x);
 						break;  
 					default:  
 					}  
 				}  
-				System.out.println("");  
+				list.add(subList);
 			}
-			FileOutputStream fos = new FileOutputStream("C:\\Users\\ishra\\eclipse-workspace\\humbi\\src\\Output.xlsx");
+	        
+	        
+	        Sheet opsheet = workbook.createSheet("Sheet1");		
+			int i = 0;
+			Row oprow = opsheet.createRow(i);
+			Cell opcell = oprow.createCell(0);
+	        int count=1;
+	        int listLen = list.size();
+	        int j = 0;
+	        int subListIndex = 0;
+	        
+	        opcell.setCellValue(list.get(0).get(0));
+			oprow = opsheet.createRow(i++);
+			opcell = oprow.createCell(0);
+	        
+	        while(subListIndex < list.get(0).size()) {
+	        	for(int lindex = 0; lindex < list.size(); lindex++) {
+	        		
+	        		List<Integer> currentSublist = list.get(lindex);
+	                if (subListIndex < currentSublist.size()) {
+	                	System.out.println(list.get(lindex).get(subListIndex)+" "+count++);
+			        	opcell.setCellValue(list.get(lindex).get(subListIndex));
+						oprow = opsheet.createRow(i++);
+						opcell = oprow.createCell(0);
+	                }
+	                else {
+	                	break;
+	                }
+		        }
+	        	subListIndex++;
+	        }
+	        FileOutputStream fos = new FileOutputStream("C:\\Users\\mohammed.ishrath\\git\\Automate\\humbi\\src\\Output.xls");
 	        workbook.write(fos);
 	        fos.close();
 		}  
